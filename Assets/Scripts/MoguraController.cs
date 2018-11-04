3ef9662bdd60ebc8e423e33f43963dc1b4a3e017
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-11-04 17:12:56 +0900

    もぐらの移動を滑らか化&コメント追加、モグラprefab化で複数設置

--- Assets/Scripts/MoguraController.cs
+++ Assets/Scripts/MoguraController.cs
@@ -8,7 +8,8 @@ public class MoguraController : MonoBehaviour {
     private bool    bHit        = false;    //もぐらが叩かれた
     private bool    bAppear     = false;    //もぐらが現れてる
     private bool    bAct        = false;    //もぐら行動中
-    private float   fMoveTime   = 0.5f;     //移動時間
+    private float   fMoveWait   = 0.5f;     //移動時間
+    private float currentWait   = 0.0f;     //移動待ち時間
     private float moveSpeed     = 0.2f;     //移動速度
     private float fRandomTime;              //もぐら入出判定時間
     private float currentTime;              //判定用の時間
@@ -43,27 +44,54 @@ public class MoguraController : MonoBehaviour {
 	}
 
     //もぐら出現処理
+    /*
+     * ①bActとtrueにして動作の重複制御
+     * ②移動時間が指定時間に達するまでY軸移動
+     * ③次の移動判定時間を乱数で指定
+     * ④移動時間と移動待ち時間を初期化
+     * ⑤bActを初期化、出現フラグをtrueにする
+     */
     private IEnumerator MoguraUp()
     {
         bAct = true;
         Debug.Log("出現");
-        transform.Translate(0,+1.1f,0);
-        yield return new WaitForSeconds(fMoveTime);
+        // ループ
+        while (currentWait <= fMoveWait)
+        {
+            transform.Translate(0, 0.05f, 0);
+            yield return new WaitForSeconds(0.025f);
+            currentWait += 0.025f;
+        }
+        
         fRandomTime = Random.Range(1.5f, 3.5f);
+        currentWait = 0f;
         currentTime = 0f;
         bAct = false;
         bAppear = true;
+
     }
 
     //もぐら隠れる処理
+    /*
+    * ①bActとtrueにして動作の重複制御
+    * ②移動時間が指定時間に達するまでY軸移動
+    * ③次の移動判定時間を乱数で指定
+    * ④移動時間と移動待ち時間を初期化
+    * ⑤bActを初期化、出現フラグをfalseにする
+    */
     private IEnumerator MoguraDown()
     {
         bAct = true;
         Debug.Log("隠れる");
-        transform.Translate(0, -1.1f, 0);
+        while (currentWait <= fMoveWait)
+        {
+            transform.Translate(0, -0.05f, 0);
+            yield return new WaitForSeconds(0.025f);
+            currentWait += 0.025f;
+        }
         //Vector3.Lerp(oldVelocity, velocity, moveSpeed * Time.deltaTime);
-        yield return new WaitForSeconds(fMoveTime);
         fRandomTime = Random.Range(0.5f, 1.5f);
+        currentWait = 0f;
         currentTime = 0f;
         bAct = false;
         bAppear = false;
